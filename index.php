79da1c066f60dcbd7971e5c01e0ac4f69cea090a
dani24gh <158100466+dani24gh@users.noreply.github.com>
2024-11-11 13:48:06 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+	<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reloj Digital</title>
+    <style>
+        
+ 
+
+        @font-face {
+            font-family: 'SevenSegment';
+            src: url('Seven Segment.ttf') format('truetype');
+        }
+
+        body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+}
+
+
+
+    </style>
+</head>
+<body>
+
+</body>
+</html>
