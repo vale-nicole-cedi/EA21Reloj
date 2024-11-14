38fde2806f70294707db8e3c2ee1e3f2009a7fc9
Sigalaeq <158208446+Sigalaeq@users.noreply.github.com>
2024-11-14 08:47:38 -0600

    Update index.php
    le puse una imagen que sea del mismo color del fondo, navbar para el titulo del mismo color del fondo y misma tipografia para el saludo y el titulo

--- index.php
+++ index.php
@@ -27,15 +27,28 @@
         color: #333;
     }
 
-    #saludo {
+    #saludo , #nav {
         font-family: Arial, sans-serif;
         font-size: 2rem;
         font-weight: bold;
         color: #555;
     }
+
 </style>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+<!-- aqui empieza -->
+<!-- As a heading -->
+<nav class="navbar" id="nav">
+  <div class="container-fluid" id="nav">
+    <span class="navbar-brand mb-0 "> <h1 id="nav">Reloj Global del Navegador</h1></span>
+  </div>
+</nav>
+<img src="https://cdn-icons-png.flaticon.com/512/2972/2972562.png">
+
+<!-- aqui termina -->
+
 <h1 id="hora"></h1>
 <h2 id="saludo"></h2>
 <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
@@ -76,6 +89,7 @@ function a() {
 
             $('#saludo').text(mensaje);
             $('body').css('background-color', bgColor);
+            $('#nav').css('background-color', bgColor); // Sincroniza el fondo del navbar con el cuerpo
         },
         error: function() {
             console.error("Error al obtener la hora del servidor.");
